81062d66261c4d7b65ea5474ed71495024515712
RepH3x <57646529+RepH3x@users.noreply.github.com>
2019-11-19 20:15:44 -0800

    comment main.java

--- src/Scripts/Main.java
+++ src/Scripts/Main.java
@@ -11,6 +11,7 @@ import org.rspeer.script.task.TaskScript;
 @ScriptMeta(name = "MiningScript", desc = "My first script!", developer = "Reph3x", category = ScriptCategory.MINING)
 public class Main extends TaskScript {
 
+	//Toggling this boolean is what switches between copper and tin
     public static boolean oreToggle = true;
 
     public static void main(String[] args) {
@@ -19,6 +20,16 @@ public class Main extends TaskScript {
 
     @Override
     public void onStart() {
+        //Submit the tasks. Tasks on top have higher priority than tasks on bottom.
+        //If a tasks' validate function returns true, that tasks execute function will be
+        //ran on a loop while the validate function is still true.
+        //If a validate function is false, it moves on the the next task.
+        //If the final tasks' validate function is false, it tries the first again, so on.
+        //
+        //Top priority is banking. If our inventory is full, bank. no exceptions.
+        //Traverse next, If we are not at a mine area and our inventory isn't full,
+        //we are running to a mine.
+        //For mining, if we are at a mine and our inventory isn't full, we mining.
         submit(
             new BankTask(),
             new Traverse(),
@@ -31,6 +42,7 @@ public class Main extends TaskScript {
 
     }
 
+	//Hamdy function to easily flip the copper/tin ore toggle
     public static void toggleOre() {
         oreToggle = !oreToggle;
     }
