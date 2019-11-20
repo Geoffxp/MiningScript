eb9e0aa4bb082f52a361a4272bc116a7c56abe46
RepH3x <57646529+RepH3x@users.noreply.github.com>
2019-11-19 20:18:23 -0800

    Comment location.java

--- src/Data/Location.java
+++ src/Data/Location.java
@@ -3,6 +3,12 @@ package Data;
 import org.rspeer.runetek.api.movement.position.Area;
 
 public enum Location {
+	//enums are kinda confusing. I don't really understand them
+	//that much. The tutorial told me to make a get function
+	//even though I don't use this.
+	//Just copy this lol and you can add more locations
+	//to this list below and get the area by calling
+	//Location.area_name (i.e. Location.coal_mine)
     copper_tin_iron_mine(Area.rectangular(3281, 3370, 3290, 3360)),
     coal_mine(Area.rectangular(3029, 9821, 3033, 9827));
 
