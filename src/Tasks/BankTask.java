e924f8c945bea46bc73259d143f2467edd2d45c7
RepH3x <57646529+RepH3x@users.noreply.github.com>
2019-11-19 20:16:33 -0800

    Comment banktask.java

--- src/Tasks/BankTask.java
+++ src/Tasks/BankTask.java
@@ -11,6 +11,11 @@ import org.rspeer.script.task.Task;
 public class BankTask extends Task {
 
 
+     //We only want to bank if our inventory is full, or if the bank is already open.
+     //If we don't check if the bank is open, this task won't be ran as soon as we
+     //deposit a single item.
+     //Remember, when this function returns true, the execute function below
+    //will be ran.
     @Override
     public boolean validate() {
         return Inventory.isFull() || Bank.isOpen();
@@ -18,17 +23,24 @@ public class BankTask extends Task {
 
     @Override
     public int execute() {
+        //Makes the player run if our energy is 25-50(random every time it's checked),
+        //and we are currently not running
         if(Movement.getRunEnergy() > Random.nextInt(25,50) && !Movement.isRunEnabled()) {
             Movement.toggleRun(true);
         }
+        //Makes the player run if his health bar is visible no matter what
+        //i.e. when we are hit
         if(Players.getLocal().isHealthBarVisible()) {
             Movement.toggleRun(true);
         }
 
         Bank.open();
         if (Bank.isOpen()) {
+            //Deposit everything in our inventory besides any pickaxe
             Bank.depositAllExcept(1265, 1267, 1269, 1271, 1273, 1275, 11920);
+            //This is when we toggle which ore we are mining.
             Main.toggleOre();
+            //Make sure you close the bank lol
             Bank.close();
         }
 
